951469ab7ad5eb4403965863b9e5c248cf85e41d
diegoxter <diegoxter1006@gmail.com>
2023-02-06 05:45:40 -0300

    Small to do list

--- src/interactions/erc20.js
+++ src/interactions/erc20.js
@@ -1,2 +1,10 @@
 import { ethers } from "ethers";
 import erc20ABI from '../assets/abis/ERC20.json'
+
+/* TO DO functions
+*
+* Check if a token exists (via symbol?)
+* Check the allowance 
+* Approve tokens
+*
+*/
