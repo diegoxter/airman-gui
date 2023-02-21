a703519f4bc7b7d39b212edf242d0d8090378de6
Diego Jose <diegoxter1006@gmail.com>
2023-02-21 17:52:43 -0300

    Refactoring

---
+++ src/components/AdminPanel/index.js
@@ -0,0 +1,59 @@
+import { Grid, Card, Segment, Header, Divider } from 'semantic-ui-react';
+import { LoadingCardGroup, NotConnectedMessage } from '../CommonComponents';
+import AdminPanelModal from './DeployAirmanModal';
+import { DeployedAirManList } from './DeployedAirManList';
+
+const AirdropManagerTab = ({network, accounts, isConnected, checkedInstances, setCheckedInstances, instances}) => {
+  return(
+    <Grid divided='vertically' container>
+      <Grid.Row>
+        <Card style={{width: '528px'}} >
+          <Grid celled='internally'>
+          <Grid.Column width={12}>
+
+          <Card.Content>
+            <Header>Deploy a new Airdrop Manager</Header>
+            <Card.Description>
+              Create a new Airdrop Manager for your community!
+            </Card.Description>
+          </Card.Content>
+          </Grid.Column>
+
+          <Grid.Column width={3} >
+          <AdminPanelModal 
+            network={ network } 
+            accounts={ accounts } 
+            isConnected={ isConnected }
+            setCheckedInstances={ setCheckedInstances }
+          /> 
+          </Grid.Column>
+        </Grid>
+      </Card>
+    </Grid.Row>
+
+    <Grid.Row>
+      {(isConnected)
+      ?
+      <DeployedAirManList 
+        network={ network } 
+        accounts={ accounts }
+        isConnected={ isConnected }
+        instances={ instances }
+        checkedInstances={ checkedInstances }
+      />
+      :
+      <Grid.Column >
+        <Segment>
+          <NotConnectedMessage />
+          <Divider hidden />
+          <LoadingCardGroup />
+        </Segment>
+      </Grid.Column>
+      }
+
+      </Grid.Row>
+    </Grid>
+  );
+}
+
+export default AirdropManagerTab
